13bcdd38c77ca679c59fe6e35bca3f5fa35810b0
Rizakura0110 <rasakura0110@gmail.com>
2023-03-09 23:41:36 +0900

    ch5end

---
+++ ch5/scripts/json_parse.js
@@ -0,0 +1,8 @@
+let data = `{
+    "isbn": "978-4-297-12635-3",
+    "title": "TypeScript入門",
+    "price": "2948"
+}`;
+
+let obj = JSON.parse(data);
+console.log(obj);
---
+++ ch5/scripts/str_len_pair.js
@@ -0,0 +1,2 @@
+let str = '叱る';
+console.log([...str].length);
