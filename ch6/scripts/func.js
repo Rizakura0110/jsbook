82558354efd663a9776b925abb74b7d9d06c0187
Rizakura0110 <rasakura0110@gmail.com>
2023-03-12 18:01:20 +0900

    6-4

--- ch6/scripts/func.js
+++ ch6/scripts/func.js
@@ -22,3 +22,38 @@ console.log(getTriangleArea4(80, 16));
 function getTriangleArea4(base, height){
     return base * height / 2;
 }
+
+function sum(...nums){
+    let result = 0;
+    for (let num of nums){
+        if(typeof num !== 'number'){
+            throw new Error(`指定値が数値ではない`);
+        }
+        result += num;
+    }
+    return result;
+}
+console.log(sum(1,2,3,4,5));
+
+function printf(format, ...args){
+    for(let i = 0; i<args.length;i++){
+        let pattern = new RegExp(`\\{${i}\\}`,'g');
+        format = format.replaceAll(pattern, args[i]);
+    }
+    console.log(format);
+}
+printf('こんにちは、{0}さん。私は{1}です。', '田中', '山田');
+
+function showDialog({
+    content = '',
+    title = 'dialog',
+}){
+    console.log(`content:${content}`);
+    console.log(`title:${title}`);
+}
+showDialog({
+    content:'だいあぉぐ',
+});
+showDialog({
+    title:'たいとる'
+});
