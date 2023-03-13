7a2ffc19f5934a5f9a053577ee639ce1c48f7c2c
Rizakura0110 <rasakura0110@gmail.com>
2023-03-13 23:29:00 +0900

    ch7end

--- ch7/scripts/object.js
+++ ch7/scripts/object.js
@@ -5,3 +5,49 @@ member.show = function(){
     console.log(`私は${this.name}、${this.age}です`);
 }
 member.show();
+
+let pet = {
+    typw : "スノーホワイトハムスター",
+    name : 'たろう',
+    description : {
+        bitrh : '2022-01-01'
+    }
+};
+let pet2 = {
+    name : '山田吉良',
+    clor : 'white',
+    description : {
+        food : 'ヒマワリの種',
+    },
+};
+let pet3 = {
+    weight : 42,
+    photo: 'hoge/hege/hoge',
+};
+Object.assign(pet, pet2, pet3);
+console.log(pet)
+
+let copied = Object.assign({}, pet);
+console.log(pet);
+console.log(copied);
+console.log(pet === copied); // false
+
+let human = {
+    name : '鈴木',
+    age : 18,
+};
+Object.defineProperty(human, 'age', {
+    value : 25,
+    writable : false,
+    configurable : true,
+    enumerable : true,
+});
+Object.defineProperty(human, 'gender', {
+    value : 'male',
+    writable : true,
+    configurable : true,
+    enumerable : true,
+});
+for (let prop in human){
+    console.log(`${prop}: ${human[prop]}`);
+};
